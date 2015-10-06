000804af1a120c3ddb7f1818b205327c4c694ec0
Bogdan Brinza <bogdan.brinza@gmail.com>
2015-10-05 23:24:17 -0700

    Few more bundles + default.html changes to hide bundled products, while caching title for search, updated and owned information. The remaining work is finishing manual bundling on owned products and changing GameDetails to show bundled products, required products and few other things when available

--- GoodOfflineGames/GoodOfflineGames/bundles.js
+++ GoodOfflineGames/GoodOfflineGames/bundles.js
@@ -1,7 +1,28 @@
 ﻿var bundles = [
     {
+        // Ultimate DOOM, The
+        "parent": 1440164514,
+        "children": [1435827232]
+    },
+    {
         // DOOM II + Final DOOM
         "parent": 1440161275,
         "children": [1435848742, 1435848814]
+    },
+    {
+        // Interstate '76 Arsenal, The 
+        "parent": 1207658836,
+        "children": [1207661003, 1207661023]
+    },
+    {
+        // System Shock: Enhanced Edition
+        "parent": 1442477571,
+        "children": [1439995156, 1442473063]
+    },
+    {
+        // Quake: The Offering
+        "parent": 1440166133,
+        "children": [1435828198]
     }
+
 ];
