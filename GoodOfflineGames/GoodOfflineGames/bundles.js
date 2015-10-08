ac0e0211bd3db98db94fd42fed5fdb9f0be43ea7
Bogdan Brinza <bogdan.brinza@gmail.com>
2015-10-07 22:18:03 -0700

    Few more bundles (some incomplete, since I don't own products and don't see all parts). Updates to styles and handling of bundles - e.g. bundle is bold, wishlisted is bold, not italic, partially owned bundle is bold italic gray.

--- GoodOfflineGames/GoodOfflineGames/bundles.js
+++ GoodOfflineGames/GoodOfflineGames/bundles.js
@@ -23,6 +23,102 @@
         // Quake: The Offering
         "parent": 1440166133,
         "children": [1435828198]
+    },
+    {
+        // Heroes of Might and Magic® 5: Bundle
+        "parent": 1207659022,
+        "children": [1207661143, 1207661193]
+    },
+    {
+        // Might and Magic® 6-pack Limited Edition
+        "parent": 1207658788,
+        "children": [1207661203, 1207661213, 1207661223, 1207661233, 1207661253, 1207661243]
+    },
+    {
+        // Quake II: Quad Damage
+        "parent": 1443696817,
+        "children": [1441704824]
+    },
+    {
+        // Quake III: Gold
+        "parent": 1443696086,
+        "children": [1441704920]
+    },
+    {
+        // Return to Castle Wolfenstein
+        "parent": 1443699418,
+        "children": [1443699418]
+    },
+    {
+        // Wolfenstein 3D + Spear of Destiny
+        "parent": 1441705226,
+        "children": [1441705046, 1441705126]
+    },
+    {
+        // STASIS: Deluxe Edition
+        "parent": 1440603706,
+        "children": [1439548178, 1440603896]
+    },
+    {
+        // Pillars of Eternity: The White March - Expansion Pass
+        "parent": 1439805257,
+        "children": [1439805122]
+    },
+    {
+        // Shadowrun: Hong Kong Deluxe Edition
+        "parent": 1440061546,
+        "children": [1436866438, 1439900139]
+    },
+    {
+        // Cities in Motion Collection
+        "parent": 1437047329,
+        "children": [1437043994, 1437046838]
+    },
+    {
+        // Pajama Sam Vol. 1
+        "parent": 1434542135,
+        "children": [1434535284, 1434535097, 1435735429]
+    },
+    {
+        // Pajama Sam Vol. 2
+        "parent": 1435136567,
+        "children": [1434536205, 1434538201, 1434538373]
+    },
+    {
+        // Ronin: Digital Special Edition
+        "parent": 1435570512,
+        "children": [1435570424, 1435570651]
+    },
+    {
+        // D4: Dark Dreams Don't Die - Season One - Deluxe Edition
+        "parent": 1433254764,
+        "children": [1433153796, 1433254891]
+    },
+    {
+        // Technobabylon: Deluxe Edition
+        "parent": 1430135783,
+        "children": [1430234218, 1430234440]
+    },
+    {
+        // Witcher 3 Wild Hunt, The
+        "parent": 1207664663,
+        "children": [1207664643]
+    },
+    {
+        // Not A Hero: Global MegaLord Edition
+        "parent": 1429699401,
+        "children": [1429699449, 1429698467]
+    },
+    {
+        // Toren Deluxe Edition
+        "parent": 1430300061,
+        "children": [1429865580]
+    },
+    {
+        // Stealth Inc. 2: A Game of Clones Deluxe
+        "parent": 1430386162,
+        "children": [1430297341]
     }
+
 
 ];
