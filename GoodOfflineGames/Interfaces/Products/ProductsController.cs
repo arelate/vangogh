b23d85f62f082928d750f350192dfea7e778def8
boggydigital <Bogdan.Brinza@gmail.com>
2016-10-23 22:26:06 -0700

    Starting to move to productsController

---
+++ GoodOfflineGames/Interfaces/Products/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+
+namespace Interfaces.Products
+{
+    public interface ILoadProductsListDelegate
+    {
+        Task LoadProductsList();
+    }
+
+    public interface IGetProductByIdDelegate<Type>
+    {
+        Task<Type> GetProductById(long id);
+    }
+
+    public interface IUpdateProductDelegate<Type>
+    {
+        Task UpdateProduct(Type product);
+    }
+
+    public interface IRemoveProductDelegate<Type>
+    {
+        Task RemoveProduct(Type product);
+    }
+
+    public interface IContainsProductDelegate<Type>
+    {
+        bool Contains(Type product);
+    }
+
+    public interface IProductsController<Type>:
+        ILoadProductsListDelegate,
+        IGetProductByIdDelegate<Type>,
+        IUpdateProductDelegate<Type>,
+        IRemoveProductDelegate<Type>,
+        IContainsProductDelegate<Type>
+    {
+        // ...
+    }
+}
