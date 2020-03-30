c066eb460f802d569fce1cc61f129309009ab6a8
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2020-03-29 21:51:02 -0700

    WIP conversion of networkController to delegates

---
+++ Delegates/Convert/Network/ConvertHttpRequestToHttpResponseAsyncDelegate.cs
@@ -0,0 +1,30 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Interfaces.Delegates.Convert;
+using Interfaces.Delegates.GetInstance;
+
+namespace Delegates.Convert.Network
+{
+    public class ConvertHttpRequestToHttpResponseAsyncDelegate :
+        IConvertAsyncDelegate<HttpRequestMessage, Task<HttpResponseMessage>>
+    {
+        private readonly IGetInstanceDelegate<HttpClient> getHttpClientInstanceDelegate;
+
+        public ConvertHttpRequestToHttpResponseAsyncDelegate(
+            IGetInstanceDelegate<HttpClient> getHttpClientInstanceDelegate)
+        {
+            this.getHttpClientInstanceDelegate = getHttpClientInstanceDelegate;
+        }
+
+        public async Task<HttpResponseMessage> ConvertAsync(HttpRequestMessage requestMessage)
+        {
+            var httpClient = getHttpClientInstanceDelegate.GetInstance();
+            var response = await httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead);
+
+            response.EnsureSuccessStatusCode();
+
+            return response;
+        }
+    }
+}
--- vangogh.Console/Program.cs
+++ vangogh.Console/Program.cs
@@ -81,6 +81,8 @@ namespace vangogh.Console
 
                 foreach (var typeDependency in typesDependencies[type])
                 {
+                    // if (typeDependency == "Controllers.Logs.ActionLogController,Controllers") continue;
+
                     if (typeDependency.StartsWith("Controllers") ||
                         typeDependency.StartsWith("GOG.Controllers"))
                         hasControllerDependencies = true;
