21a9edc7a510c6c99ef1d565231aca133f731194
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2020-04-30 18:35:12 -0700

    Restore program main to continue debugging app logic

--- vangogh.Console/Program.cs
+++ vangogh.Console/Program.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Interfaces.Delegates.Respond;
+using Interfaces.Delegates.Convert;
 using Delegates.Convert.Requests;
 using Delegates.Convert.Types;
 
@@ -13,33 +14,42 @@ namespace vangogh.Console
     {
         private static async Task Main(string[] args)
         {
-            // // DEBUG
-            // args = new string[] { "update", "accountproducts" };
+            // DEBUG
+            args = new string[] {"update", "products"};
 
-            // var singletonInstancesController = new SingletonInstancesController();
+            var convertTypeToDependenciesConstructorInfoDelegate =
+                new ConvertTypeToDependenciesConstructorInfoDelegate();
 
-            // var convertArgsToRequestsDelegate = singletonInstancesController.GetInstance(
-            //     typeof(ConvertArgsToRequestsDelegate))
-            //     as ConvertArgsToRequestsDelegate;
+            var convertConstructorInfoToDependenciesTypesDelegate =
+                new ConvertConstructorInfoToDependenciesTypesDelegate();
 
-            // var convertRequestToRespondDelegateTypeDelegate = singletonInstancesController.GetInstance(
-            //     typeof(ConvertRequestToRespondDelegateTypeDelegate))
-            //     as ConvertRequestToRespondDelegateTypeDelegate;
+            var convertTypeToInstanceDelegate =
+                new ConvertTypeToInstanceDelegate(
+                    convertTypeToDependenciesConstructorInfoDelegate,
+                    convertConstructorInfoToDependenciesTypesDelegate);
 
-            // await foreach (var request in convertArgsToRequestsDelegate.ConvertAsync(args))
-            // {
-            //     var respondToRequestDelegateType = convertRequestToRespondDelegateTypeDelegate.Convert(request);
+            var convertArgsToRequestsDelegate = convertTypeToInstanceDelegate.Convert(
+                    typeof(ConvertArgsToRequestsDelegate))
+                as ConvertArgsToRequestsDelegate;
 
-            //     if (respondToRequestDelegateType == null)
-            //         throw new System.InvalidOperationException(
-            //             $"No respond delegate registered for request: {request.Method} {request.Collection}");
+            var convertRequestToRespondDelegateTypeDelegate = convertTypeToInstanceDelegate.Convert(
+                    typeof(ConvertRequestToRespondDelegateTypeDelegate))
+                as ConvertRequestToRespondDelegateTypeDelegate;
 
-            //     var respondToRequestDelegate = singletonInstancesController.GetInstance(
-            //         respondToRequestDelegateType)
-            //         as IRespondAsyncDelegate;
+            await foreach (var request in convertArgsToRequestsDelegate.ConvertAsync(args))
+            {
+                var respondToRequestDelegateType = convertRequestToRespondDelegateTypeDelegate.Convert(request);
 
-            //     // await respondToRequestDelegate.RespondAsync(request.Parameters);
-            // }
+                if (respondToRequestDelegateType == null)
+                    throw new System.InvalidOperationException(
+                        $"No respond delegate registered for request: {request.Method} {request.Collection}");
+
+                var respondToRequestDelegate = convertTypeToInstanceDelegate.Convert(
+                        respondToRequestDelegateType)
+                    as IRespondAsyncDelegate;
+
+                await respondToRequestDelegate.RespondAsync(request.Parameters);
+            }
 
             System.Console.WriteLine("Press ENTER to exit...");
             System.Console.ReadLine();
