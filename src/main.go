dbb25bfdb8f58ed7a70a7575be71597230651dab
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2020-08-02 15:03:55 -0700

    Minor initial cleanup

--- src/main.go
+++ src/main.go
@@ -1,1 +1,198 @@
-package src
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang.org/x/net/html"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const (
+	loginHost = "https://login.gog.com"
+	loginURL = loginHost + "/login"
+	loginCheckURL = loginHost + "/login_check"
+	loginTwoStepURL = loginHost + "/login/two_step"
+	userDataURL = "https://gog.com/userData.json"
+)
+
+func GetAttrVal(node *html.Node, key string) string {
+	for _, attr := range node.Attr {
+		if attr.Key == key {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func GetElementByTagAttrVal(doc *html.Node, data, key, val string) *html.Node {
+	var f func(*html.Node) *html.Node
+	f = func(n *html.Node) *html.Node {
+		if n.Type == html.ElementNode && n.Data == data {
+			if GetAttrVal(n, key) == val {
+				return n
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			val := f(c)
+			if val != nil {
+				return val
+			}
+		}
+		return nil
+	}
+	return f(doc)
+}
+
+func DefaultHeaders(req *http.Request) {
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "en-us")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15")
+}
+
+func AuthToken(client *http.Client) string {
+
+	req, _ := http.NewRequest(http.MethodGet, loginURL, nil)
+	DefaultHeaders(req)
+	req.Host = "login.gog.com"
+
+	resp, _ := client.Do(req)
+
+	defer resp.Body.Close()
+
+	doc, _ := html.Parse(resp.Body)
+
+	// check for captcha presence
+	if GetElementByTagAttrVal(doc, "div", "class", "g-recaptcha form__recaptcha") != nil {
+		panic("Captcha present on the page")
+	}
+
+	return GetAttrVal(
+		GetElementByTagAttrVal(doc, "input", "name", "login[_token]"), "value")
+}
+
+func GetUserData(prompt string) string {
+	fmt.Print(prompt)
+	val := ""
+	fmt.Scanln(&val)
+	return val
+}
+
+func Authorize(client *http.Client, username, password string) {
+
+	token := AuthToken(client)
+
+	if username == "" {
+		username = GetUserData("Please enter your GOG.com username:")
+	}
+	if password == "" {
+		password = GetUserData("Please enter password for " + username + ": ")
+	}
+
+	data := url.Values {
+		"login[username]": {username},
+		"login[password]": {password},
+		"login[login_flow]": {"default"},
+		"login[_token]": {token},
+	}
+	sdata := data.Encode()
+
+	req, _ := http.NewRequest(http.MethodPost, loginCheckURL, strings.NewReader(sdata))
+	DefaultHeaders(req)
+	req.Host = "login.gog.com"
+	req.Header.Set("Referer", loginURL)
+	req.Header.Set("Origin", "https://login.gog.com")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, _ := client.Do(req)
+
+	defer resp.Body.Close()
+
+	doc, _ := html.Parse(resp.Body)
+
+	if GetElementByTagAttrVal(doc, "input", "id", "second_step_authentication_token_letter_1") != nil {
+
+		// Server is requesting second factor authentication
+		token = GetAttrVal(
+			GetElementByTagAttrVal(doc, "input", "name", "second_step_authentication[_token]"), "val")
+		fmt.Println(token)
+
+		code := "";
+		for len(code) != 4 {
+			code = GetUserData("Please enter 2FA code (check your inbox):")
+		}
+
+		data = url.Values {
+			"second_step_authentication[token][letter_1]": {string(code[0])},
+			"second_step_authentication[token][letter_2]": {string(code[1])},
+			"second_step_authentication[token][letter_3]": {string(code[2])},
+			"second_step_authentication[token][letter_4]": {string(code[3])},
+			"second_step_authentication[_token]": {token},
+		}
+		sdata = data.Encode()
+
+		req, _ := http.NewRequest(http.MethodPost, loginTwoStepURL, strings.NewReader(sdata))
+		DefaultHeaders(req)
+		req.Host = "login.gog.com"
+		req.Header.Set("Referer", loginTwoStepURL)
+		req.Header.Set("Origin", loginHost)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		client.Do(req)
+
+		fmt.Println(IsLoggedIn(client))
+
+		return
+	}
+
+	fmt.Println(IsLoggedIn(client))
+}
+
+type UserData struct {
+	IsLoggedIn bool `json:"isLoggedIn"`
+}
+
+func IsLoggedIn(client *http.Client) bool {
+
+	req, _ := http.NewRequest(http.MethodGet, userDataURL, nil)
+	DefaultHeaders(req)
+
+	resp, _ := client.Do(req)
+
+	defer resp.Body.Close()
+
+	respBody, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(string(respBody))
+
+	//return false
+
+	var userData UserData
+
+	json.Unmarshal(respBody, &userData)
+
+	return userData.IsLoggedIn
+}
+
+func main() {
+
+	cookieJar, _ := cookiejar.New(nil)
+
+	client := http.Client{
+		Timeout: time.Minute * 1,
+		Jar: cookieJar,
+	}
+
+	Authorize(&client, "", "")
+
+	// test output - we'll need to preserve cookies in the future between sessions
+	// which also should allow to reuse browser cookies
+	for _, cookie := range client.Jar.Cookies(&url.URL{Scheme: "https", Host: "login.gog.com"}) {
+		fmt.Println(cookie)
+	}
+}
