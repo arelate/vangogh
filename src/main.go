ca24486869ce9b0a7f5fbf88c9ac8a15f1495165
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2020-08-02 22:09:10 -0700

    More clean-up and refactor

--- src/main.go
+++ src/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"golang.org/x/net/html"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/cookiejar"
@@ -24,6 +26,9 @@ const (
 )
 
 func GetAttrVal(node *html.Node, key string) string {
+	if node == nil {
+		return ""
+	}
 	for _, attr := range node.Attr {
 		if attr.Key == key {
 			return attr.Val
@@ -58,20 +63,37 @@ func DefaultHeaders(req *http.Request) {
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15")
 }
 
-func getAuthToken(client *http.Client) string {
+func getAuthTokenAttrVal(doc *html.Node) string {
+	input := GetElementByTagAttrVal(doc, "input", "name", "login[_token]")
+	return GetAttrVal(input, "value")
+}
 
-	clientId := "46755278331571209"
-	redirectUri := "https://www.gog.com/on_login_success"
-	responseType := "code"
-	layout := "default"
-	brand := "gog"
-	gogLc := "en-US"
+func getSecondStepAuthTokenAttrVal(doc *html.Node) string {
+	input := GetElementByTagAttrVal(doc, "input", "name", "second_step_authentication[_token]")
+	return GetAttrVal(input, "value")
+}
+
+func getCaptchaElement(doc *html.Node) *html.Node {
+	return GetElementByTagAttrVal(doc, "div", "class", "g-recaptcha form__recaptcha")
+}
+
+func getAuthURL(host string) *url.URL {
+
+	const (
+		clientId     = "46755278331571209"
+		redirectUri  = "https://www.gog.com/on_login_success"
+		responseType = "code"
+		layout       = "default"
+		brand        = "gog"
+		gogLc        = "en-US"
+	)
 
 	authURL := url.URL{
 		Scheme: scheme,
-		Host:   authHost,
+		Host:   host,
 		Path:   authPath,
 	}
+
 	q := authURL.Query()
 	q.Set("client_id", clientId)
 	q.Set("redirect_uri", redirectUri)
@@ -81,46 +103,125 @@ func getAuthToken(client *http.Client) string {
 	q.Set("gog_lc", gogLc)
 	authURL.RawQuery = q.Encode()
 
-	req, _ := http.NewRequest(http.MethodGet, authURL.String(), nil)
+	return &authURL
+}
+
+func getAuthToken(client *http.Client) (token string, error error) {
+
+	authURL := getAuthURL(authHost)
+
+	req, err := http.NewRequest(http.MethodGet, authURL.String(), nil)
+
+	if err != nil {
+		return "", err
+	}
+
 	DefaultHeaders(req)
 	req.Host = authHost
 
-	resp, _ := client.Do(req)
-
+	resp, err := client.Do(req)
 	defer resp.Body.Close()
 
-	doc, _ := html.Parse(resp.Body)
-
-	// check for captcha presence
-	if GetElementByTagAttrVal(doc, "div", "class", "g-recaptcha form__recaptcha") != nil {
-		panic("Captcha present on the page")
+	if err != nil {
+		return "", err
 	}
 
-	return GetAttrVal(
-		GetElementByTagAttrVal(
-			doc,
-			"input",
-			"name",
-			"login[_token]"),
-		"value")
+	doc, err := html.Parse(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	// check for captcha presence
+	if getCaptchaElement(doc) != nil {
+		// TODO: When cookie persistence is implemented - add reference to allow user to unblock themselves
+		return "", errors.New("captcha present on the page")
+	}
+
+	return getAuthTokenAttrVal(doc), nil
 }
 
-func GetUserData(prompt string) string {
+func requestUserData(prompt string) string {
 	fmt.Print(prompt)
 	val := ""
 	fmt.Scanln(&val)
 	return val
 }
 
-func Authorize(client *http.Client, username, password string) {
+func authorizeSecondStep(body io.ReadCloser, client *http.Client) error {
 
-	token := getAuthToken(client)
+	attempts := 3
+
+	doc, err := html.Parse(body)
+	if err != nil {
+		return err
+	}
+	token := getSecondStepAuthTokenAttrVal(doc)
+
+	for token != "" && attempts > 0 {
+
+		// Server is requesting second factor authentication
+		attempts--
+		code := ""
+		for len(code) != 4 {
+			code = requestUserData("Please enter 2FA code (check your inbox):")
+		}
+
+		data := url.Values{
+			"second_step_authentication[token][letter_1]": {string(code[0])},
+			"second_step_authentication[token][letter_2]": {string(code[1])},
+			"second_step_authentication[token][letter_3]": {string(code[2])},
+			"second_step_authentication[token][letter_4]": {string(code[3])},
+			"second_step_authentication[_token]":          {token},
+		}
+		s := data.Encode()
+
+		loginTwoStepURL := url.URL{
+			Scheme: scheme,
+			Host:   loginHost,
+			Path:   loginTwoStepPath,
+		}
+
+		req, _ := http.NewRequest(http.MethodPost, loginTwoStepURL.String(), strings.NewReader(s))
+		DefaultHeaders(req)
+		req.Host = "login.gog.com"
+		req.Header.Set("Referer", loginTwoStepURL.String())
+		req.Header.Set("Origin", loginHost)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		resp, err := client.Do(req)
+
+		if err != nil {
+			return err
+		}
+
+		doc, err = html.Parse(resp.Body)
+		if err != nil {
+			return err
+		}
+
+		token = getSecondStepAuthTokenAttrVal(doc)
+
+		resp.Body.Close()
+	}
+
+	return nil
+}
+
+// Authorize username on GOG.com for account related queries
+func Authorize(client *http.Client, username, password string) error {
+
+	token, err := getAuthToken(client)
+
+	if err != nil {
+		return err
+	}
 
 	if username == "" {
-		username = GetUserData("Please enter your GOG.com username:")
+		username = requestUserData("Please enter your username:")
 	}
 	if password == "" {
-		password = GetUserData("Please enter password for " + username + ": ")
+		password = requestUserData("Hello, " + username + "! Please enter your password:")
 	}
 
 	data := url.Values{
@@ -137,57 +238,35 @@ func Authorize(client *http.Client, username, password string) {
 		Path:   loginCheckPath,
 	}
 
-	req, _ := http.NewRequest(http.MethodPost, loginCheckURL.String(), strings.NewReader(s))
+	req, err := http.NewRequest(http.MethodPost, loginCheckURL.String(), strings.NewReader(s))
+
+	if err != nil {
+		return err
+	}
+
 	DefaultHeaders(req)
 	req.Host = loginHost
-	req.Header.Set("Referer", "https://login.gog.com/auth?brand=gog&client_id=46755278331571209&layout=default&redirect_uri=https%3A%2F%2Fwww.gog.com%2Fon_login_success&response_type=code")
-	req.Header.Set("Origin", scheme+"://"+loginHost)
+	// GOG.com redirects initial auth request from authHost to loginHost.
+	loginAuthURL := getAuthURL(loginHost)
+	req.Header.Set("Referer", loginAuthURL.String())
+	u := url.URL{Scheme: scheme, Host: loginHost}
+	req.Header.Set("Origin", u.String())
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	resp, _ := client.Do(req)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
 
 	defer resp.Body.Close()
 
-	doc, _ := html.Parse(resp.Body)
-
-	if GetElementByTagAttrVal(doc, "input", "id", "second_step_authentication_token_letter_1") != nil {
-
-		// Server is requesting second factor authentication
-		token = GetAttrVal(
-			GetElementByTagAttrVal(doc, "input", "name", "second_step_authentication[_token]"), "value")
-		fmt.Println(token)
-
-		code := ""
-		for len(code) != 4 {
-			code = GetUserData("Please enter 2FA code (check your inbox):")
-		}
-
-		data = url.Values{
-			"second_step_authentication[token][letter_1]": {string(code[0])},
-			"second_step_authentication[token][letter_2]": {string(code[1])},
-			"second_step_authentication[token][letter_3]": {string(code[2])},
-			"second_step_authentication[token][letter_4]": {string(code[3])},
-			"second_step_authentication[_token]":          {token},
-		}
-		s = data.Encode()
-
-		loginTwoStepURL := url.URL{
-			Scheme: scheme,
-			Host:   loginHost,
-			Path:   loginTwoStepPath,
-		}
-
-		req, _ := http.NewRequest(http.MethodPost, loginTwoStepURL.String(), strings.NewReader(s))
-		DefaultHeaders(req)
-		req.Host = "login.gog.com"
-		req.Header.Set("Referer", loginTwoStepURL.String())
-		req.Header.Set("Origin", loginHost)
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-		client.Do(req)
+	if err := authorizeSecondStep(resp.Body, client); err != nil {
+		return err
 	}
 
-	fmt.Println("User is logged in: ", IsLoggedIn(client))
+	//fmt.Println("User is logged in: ", IsLoggedIn(client))
+
+	return nil
 }
 
 type UserData struct {
@@ -215,40 +294,6 @@ func IsLoggedIn(client *http.Client) bool {
 func main() {
 
 	cookieJar, _ := cookiejar.New(nil)
-	//cookieJar.SetCookies(&url.URL{Scheme: scheme, Host: gogHost}, []*http.Cookie{
-	//	{
-	//		Name:     "gog-al",
-	//		Value:    "45kzx5nx3HT1j0jgyu_3WytbksBOsNFbxz-490OjV-p8EKRYhnj4URb4yekZpZgFRofW35Jw7tEm2kqe9YeGtwWRYi9Gyd7cu7h8UDdJ2i3DHxGvyCriaVUYtQD-bQ2s",
-	//		Path:     "/",
-	//		Domain:   ".gog.com",
-	//		Secure:   true,
-	//		HttpOnly: true,
-	//	},
-	//	{
-	//		Name:     "cart_token",
-	//		Value:    "a07df5d1b29af0d7",
-	//		Path:     "/",
-	//		Domain:   ".gog.com",
-	//		Secure:   true,
-	//		HttpOnly: true,
-	//	},
-	//	{
-	//		Name:     "gog_us",
-	//		Value:    "m1fe170k233t6906s5qa2c6bq3",
-	//		Path:     "/",
-	//		Domain:   ".gog.com",
-	//		Secure:   true,
-	//		HttpOnly: true,
-	//	},
-	//	{
-	//		Name:     "gog_lc",
-	//		Value:    "en-US",
-	//		Path:     "/",
-	//		Domain:   ".gog.com",
-	//		Secure:   true,
-	//		HttpOnly: true,
-	//	},
-	//})
 
 	client := http.Client{
 		Timeout: time.Minute * 2,
@@ -258,6 +303,13 @@ func main() {
 	if !IsLoggedIn(&client) {
 		Authorize(&client, "", "")
 	}
+
+	status := "isn't"
+	if IsLoggedIn(&client) {
+		status = "is"
+	}
+
+	fmt.Printf("User %s logged in\n", status)
 
 	// test output - we'll need to preserve cookies in the future between sessions
 	// which also should allow to reuse browser cookies
