02dfe408a274766d28a58b7930d958187395ac75
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-02-21 19:02:16 -0800

    Updating to 1.16

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/boggydigital/vangogh
 
-go 1.15
+go 1.16
 
 require (
 	github.com/arelate/gog_auth v0.1.3-alpha
@@ -8,7 +8,7 @@ require (
 	github.com/arelate/gog_urls v0.1.7-alpha
 	github.com/arelate/vangogh_types v0.1.0-alpha
 	github.com/arelate/vangogh_urls v0.1.0-alpha
-	github.com/boggydigital/clo v1.0.3
+	github.com/boggydigital/clo v0.2.2-alpha
 	github.com/boggydigital/dolo v0.1.4-alpha
 	github.com/boggydigital/kvas v0.1.9-alpha
 )
--- main.go
+++ main.go
@@ -5,36 +5,21 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
 	"github.com/boggydigital/clo"
 	"github.com/boggydigital/vangogh/cmd"
 	"log"
 	"os"
 )
 
+//go:embed "clo.json"
+var cloBytes []byte
+
 func main() {
+	bytesBuffer := bytes.NewBuffer(cloBytes)
 
-	//f, err := os.Create(filepath.Join(os.TempDir(), logFile))
-	//log.SetOutput(f)
-	//log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	//log.Println("Started new session")
-
-	//cookies, err := session.Load()
-	//if err != nil {
-	//	fmt.Println("cannot load session cookies")
-	//}
-	//
-	//jar, err := cookiejar.New(nil)
-	//if err != nil {
-	//	fmt.Println("cannot create cookie jar")
-	//	log.Fatalln(err)
-	//}
-	//
-	//gogHost := &url.URL{Scheme: urls.HttpsScheme, Host: urls.GogHost}
-	//if jar != nil && cookies != nil {
-	//	jar.SetCookies(gogHost, cookies)
-	//}
-
-	defs, err := clo.LoadDefault("clo.json")
+	defs, err := clo.Load(bytesBuffer)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,47 +33,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	//httpClient := &http.Client{
-	//	Timeout: time.Minute * 5,
-	//	Jar:     jar,
-	//}
-
-	//err = cmd.Run(httpClient, mongoClient, ctx, os.Args)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	os.Exit(1)
-	//}
-
-	//gameDetailsOrigin := remote.NewDetails(httpClient, urls.Details(media.Game))
-	//gameProductsOrigin := remote.NewProductPage(httpClient, media.Game)
-	//
-	//detailsDest := local.NewDetails(mongoClient, ctx)
-	//productsDest := local.NewProducts(mongoClient, ctx)
-	//
-	//tp, _ := gameProductsOrigin.FetchPage(1, productsDest)
-	//fmt.Println(tp)
-	//
-	//_ = gameDetailsOrigin.Transfer(1304291300, detailsDest)
-	//
-	//_ = mongoClient.Disconnect(ctx)
-	//
-	//os.Exit(1)
-
-	//mongoClient, err := mongocl.New()
-	//if err != nil {
-	//	fmt.Println("Cannot create MongoDB client.", checkLog)
-	//	log.Fatalln(err)
-	//}
-	//
-	//if err := cli.Run(httpClient, mongoClient, os.Args); err != nil {
-	//	fmt.Println("Application has encountered an error.", checkLog)
-	//	log.Fatalln(err)
-	//}
-	//
-	//err = session.Save(httpClient.Jar.Cookies(gogHost))
-	//if err != nil {
-	//	fmt.Println("Cannot save session cookies.", checkLog)
-	//	log.Fatalln(err)
-	//}
 }
