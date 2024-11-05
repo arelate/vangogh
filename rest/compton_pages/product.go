d8c465d4622ae5e61e6a02440f6637e5d2a47987
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-11-04 20:45:13 -0800

    Staring product view with all sections closed

--- rest/compton_pages/product.go
+++ rest/compton_pages/product.go
@@ -57,11 +57,11 @@ func Product(id string, rdx kevlar.ReadableRedux, hasSections []string) compton.
 
 	/* Product details sections */
 
-	for ii, section := range hasSections {
+	for _, section := range hasSections {
 
 		sectionTitle := compton_data.SectionTitles[section]
 		summaryHeading := compton.DSTitle(p, sectionTitle)
-		detailsSummary := compton.DSLarge(p, summaryHeading, ii == 0).
+		detailsSummary := compton.DSLarge(p, summaryHeading, false).
 			BackgroundColor(color.Highlight).
 			ForegroundColor(color.Foreground).
 			MarkerColor(color.Gray).
