5b877a3938f90642d4c8d0a83a84016d84d677cf
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-11-15 13:06:01 -0800

    Enhancing downloads-metadata to return included products downloads in case of PACK products

--- rest/get_downloads.go
+++ rest/get_downloads.go
@@ -33,14 +33,47 @@ func GetDownloads(w http.ResponseWriter, r *http.Request) {
 
 func getDownloads(id string,
 	operatingSystems []vangogh_local_data.OperatingSystem,
-	languageCodes []string,
-	excludePatches bool,
+	langCodes []string,
+	noPatches bool,
 	rdx kevlar.ReadableRedux) (vangogh_local_data.DownloadsList, error) {
 
 	vrDetails, err := vangogh_local_data.NewProductReader(vangogh_local_data.Details)
 	if err != nil {
 		return nil, err
 	}
+
+	has, err := vrDetails.Has(id)
+	if err != nil {
+		return nil, err
+	}
+
+	// if we don't have product details for the id this can mean two things:
+	// - id represents a PACK that includes individual products (owned, not owned)
+	// in this case we can iterate over included products and combine downloads for each one of them
+	// - id represents a product that the user doesn't own
+	// in this case we can remove basic product metadata (title, slug, etc) and no downloads
+
+	if !has {
+		if err := rdx.MustHave(vangogh_local_data.IncludesGamesProperty); err != nil {
+			return nil, err
+		}
+		var includesDownloadsList vangogh_local_data.DownloadsList
+		if includesIds, ok := rdx.GetAllValues(vangogh_local_data.IncludesGamesProperty, id); ok {
+
+			for _, includesId := range includesIds {
+				if idl, err := getDownloads(includesId, operatingSystems, langCodes, noPatches, rdx); err == nil {
+					includesDownloadsList = append(includesDownloadsList, idl...)
+				} else {
+					return nil, err
+				}
+			}
+			return includesDownloadsList, nil
+		}
+		return includesDownloadsList, nil
+	}
+
+	// at this point we know that we should have product details in storage (see above)
+	// so if we don't - that should be an error worth investigating
 
 	det, err := vrDetails.Details(id)
 	if err != nil {
@@ -58,6 +91,6 @@ func getDownloads(id string,
 
 	return dl.Only(operatingSystems,
 		[]vangogh_local_data.DownloadType{vangogh_local_data.AnyDownloadType},
-		languageCodes,
-		excludePatches), nil
+		langCodes,
+		noPatches), nil
 }
