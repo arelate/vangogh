5841cc44a8d23bb168e55e1413b09be1025cbce6
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-11-20 16:03:53 -0800

    Fixing theo check script

--- rest/compton_pages/scripts/check_theo.js
+++ rest/compton_pages/scripts/check_theo.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", e=>{
     let theoCheck = document.querySelector(".theo-span")
     fetch("http://localhost:1857/health")
-    .then(resp => { if (resp && resp.ok) { resp.text() }})
-    .then(data => { if (data == "ok") { theoCheck.textContent = "Let there be light"}})
+    .then(resp => {
+        if (resp && resp.ok) {
+            resp.text()
+            .then(data => {
+                if (data == "ok") { theoCheck.textContent = "Let there be light"
+             }})
+        }})
     .catch(e => { theoCheck.textContent = "He who seeks shall find"})
 })
