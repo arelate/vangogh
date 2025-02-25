2d30aff593664fddf204b1b491e6909081ae7192
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-02-24 20:58:15 -0800

    Changing Installers label to use the same style as other details summary

--- rest/compton_fragments/product_validation_result.go
+++ rest/compton_fragments/product_validation_result.go
@@ -4,6 +4,8 @@ import (
 	"github.com/arelate/southern_light/vangogh_integration"
 	"github.com/boggydigital/compton"
 	"github.com/boggydigital/compton/consts/color"
+	"github.com/boggydigital/compton/consts/font_weight"
+	"github.com/boggydigital/compton/consts/size"
 	"github.com/boggydigital/redux"
 )
 
@@ -20,11 +22,21 @@ var ValidationResultsColors = map[vangogh_integration.ValidationResult]color.Col
 
 func ProductValidationResult(r compton.Registrar, id string, rdx redux.Readable) compton.Element {
 	pvrc := color.Gray
+
 	if pvrs, ok := rdx.GetLastVal(vangogh_integration.ProductValidationResultProperty, id); ok {
+
 		pvr := vangogh_integration.ParseValidationResult(pvrs)
+
+		prve := compton.Fspan(r, pvr.HumanReadableString()).
+			FontSize(size.Small).
+			FontWeight(font_weight.Normal).
+			PaddingInline(size.XSmall).
+			PaddingBlock(size.XXSmall).
+			BorderRadius(size.XXSmall)
+
 		pvrc = ValidationResultsColors[pvr]
 
-		return compton.SvgUse(r, compton.Circle).ForegroundColor(pvrc)
+		return prve.BackgroundColor(pvrc).ForegroundColor(color.Highlight)
 	}
 
 	return nil
