be2a5f92a67b3c3ed4be9cc5203d417656e9220e
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-03-06 17:29:40 -0800

    Fixing Steam Reviews using old-GOG id and not Steam App Id

---
+++ cli/shared_data/reduce_types.go
@@ -0,0 +1,89 @@
+package shared_data
+
+import (
+	"github.com/arelate/southern_light/vangogh_integration"
+	"github.com/boggydigital/kevlar"
+	"github.com/boggydigital/nod"
+	"github.com/boggydigital/pathways"
+	"github.com/boggydigital/redux"
+)
+
+func ReduceTypes() error {
+
+	ta := nod.Begin(" reducing %s...", vangogh_integration.TypesProperty)
+	defer ta.Done()
+
+	reduxDir, err := pathways.GetAbsRelDir(vangogh_integration.Redux)
+	if err != nil {
+		return err
+	}
+
+	rdx, err := redux.NewWriter(reduxDir,
+		vangogh_integration.SteamAppIdProperty,
+		vangogh_integration.TypesProperty)
+	if err != nil {
+		return err
+	}
+
+	steamAppIdGogIds := getSteamAppIdGogIds(rdx)
+
+	idsTypes := make(map[string][]string)
+
+	for pt := range vangogh_integration.AllProductTypes() {
+
+		if pt == vangogh_integration.UnknownProductType {
+			continue
+		}
+
+		ptDir, err := vangogh_integration.AbsProductTypeDir(pt)
+		if err != nil {
+			return err
+		}
+
+		kvPt, err := kevlar.New(ptDir, kevlar.JsonExt)
+		if err != nil {
+			return err
+		}
+
+		for id := range kvPt.Keys() {
+
+			if idsTypes[id] == nil {
+				idsTypes[id] = make([]string, 0)
+			}
+
+			switch pt {
+			case vangogh_integration.SteamAppDetails:
+				fallthrough
+			case vangogh_integration.SteamAppNews:
+				fallthrough
+			case vangogh_integration.SteamDeckCompatibilityReport:
+				fallthrough
+			case vangogh_integration.SteamAppReviews:
+				gogId := steamAppIdGogIds[id]
+				idsTypes[gogId] = append(idsTypes[gogId], pt.String())
+			default:
+				idsTypes[id] = append(idsTypes[id], pt.String())
+			}
+		}
+	}
+
+	if err = rdx.BatchReplaceValues(vangogh_integration.TypesProperty, idsTypes); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getSteamAppIdGogIds(rdx redux.Readable) map[string]string {
+	steamAppIdGogIds := make(map[string]string)
+
+	for gogId := range rdx.Keys(vangogh_integration.SteamAppIdProperty) {
+		if steamAppIds, ok := rdx.GetAllValues(vangogh_integration.SteamAppIdProperty, gogId); ok {
+			for _, said := range steamAppIds {
+				steamAppIdGogIds[said] = gogId
+			}
+		}
+	}
+
+	return steamAppIdGogIds
+}
--- rest/get_steam_reviews.go
+++ rest/get_steam_reviews.go
@@ -1,8 +1,12 @@
 package rest
 
 import (
+	"encoding/json"
+	"errors"
+	"github.com/arelate/southern_light/steam_integration"
 	"github.com/arelate/southern_light/vangogh_integration"
 	"github.com/arelate/vangogh/rest/compton_pages"
+	"github.com/boggydigital/kevlar"
 	"github.com/boggydigital/nod"
 	"net/http"
 )
@@ -11,24 +15,51 @@ func GetSteamReviews(w http.ResponseWriter, r *http.Request) {
 
 	// GET /steam-reviews?id
 
-	id := r.URL.Query().Get("id")
+	gogId := r.URL.Query().Get("id")
 
-	sarReader, err := vangogh_integration.NewProductReader(vangogh_integration.SteamAppReviews)
-
+	appReviews, err := getSteamReviews(gogId)
 	if err != nil {
 		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
 		return
 	}
 
-	sar, err := sarReader.SteamAppReviews(id)
-	if err != nil {
-		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
-		return
-	}
-
-	p := compton_pages.SteamReviews(sar)
+	p := compton_pages.SteamReviews(appReviews)
 
 	if err := p.Write(w); err != nil {
 		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
 	}
+}
+
+func getSteamReviews(gogId string) (*steam_integration.AppReviews, error) {
+
+	var steamAppId string
+	if sai, ok := rdx.GetLastVal(vangogh_integration.SteamAppIdProperty, gogId); ok && sai != "" {
+		steamAppId = sai
+	} else {
+		return nil, errors.New("no steam app id for gog id " + gogId)
+	}
+
+	appReviewsDir, err := vangogh_integration.AbsProductTypeDir(vangogh_integration.SteamAppReviews)
+	if err != nil {
+		return nil, err
+	}
+
+	kvAppReviews, err := kevlar.New(appReviewsDir, kevlar.JsonExt)
+	if err != nil {
+		return nil, err
+	}
+
+	rcAppReviews, err := kvAppReviews.Get(steamAppId)
+	if err != nil {
+		return nil, err
+	}
+	defer rcAppReviews.Close()
+
+	var appReviews steam_integration.AppReviews
+	if err = json.NewDecoder(rcAppReviews).Decode(&appReviews); err != nil {
+		return nil, err
+	}
+
+	return &appReviews, nil
+
 }
