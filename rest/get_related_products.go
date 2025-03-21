fd9659000ee8b033e995c30b39fabde6bd04be3a
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-03-20 17:10:02 -0700

    Adding initial plumbing for related products section (Editions, DLCs) that would display those as product cards

---
+++ rest/get_related_products.go
@@ -0,0 +1,24 @@
+package rest
+
+import (
+	"github.com/boggydigital/nod"
+	"net/http"
+)
+
+func GetRelatedProducts(w http.ResponseWriter, r *http.Request) {
+
+	// GET /related-products?id
+
+	if err := RefreshRedux(); err != nil {
+		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//id := r.URL.Query().Get("id")
+
+	//if p := compton_pages.ExternalLinks(id, rdx); p != nil {
+	//	if err := p.WriteResponse(w); err != nil {
+	//		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
+	//	}
+	//}
+}
