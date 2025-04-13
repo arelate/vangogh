arelate/vangogh

commit eb581263692714a079730634251671219e8f2710

Fixing sync crashing when user has no games in wishlist

cli/gog_data/get_user_wishlist.go
@@ -2,6 +2,7 @@ package gog_data
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/arelate/southern_light/gog_integration"
 	"github.com/arelate/southern_light/vangogh_integration"
 	"github.com/arelate/vangogh/cli/fetch"
@@ -66,16 +67,33 @@ func ReduceUserWishlist(kvUserWishlist kevlar.KeyValues) error {
 		return err
 	}
 
-	userWishlistMap := make(map[string][]string, len(userWishlist.Wishlist))
-	for id, flag := range userWishlist.Wishlist {
-		if flag {
-			userWishlistMap[id] = []string{vangogh_integration.TrueValue}
+	// GOG.com serves map[string]bool when user has some games wishlisted
+	// and an empty array when no game has been wishlisted
+	// so in order to process the data we attempt a type conversion
+	// and handle both known data types (and fail if data has another format)
+
+	if uwlMap, ok := userWishlist.Wishlist.(map[string]bool); ok {
+
+		userWishlistMap := make(map[string][]string, len(uwlMap))
+		for id, flag := range uwlMap {
+			if flag {
+				userWishlistMap[id] = []string{vangogh_integration.TrueValue}
+			}
 		}
-	}
 
-	if err = rdx.CutKeys(key, slices.Collect(rdx.Keys(key))...); err != nil {
-		return err
-	}
+		if err = rdx.CutKeys(key, slices.Collect(rdx.Keys(key))...); err != nil {
+			return err
+		}
 
-	return rdx.BatchAddValues(key, userWishlistMap)
+		return rdx.BatchAddValues(key, userWishlistMap)
+
+	} else if uwlSlice, sure := userWishlist.Wishlist.([]any); sure {
+		if len(uwlSlice) == 0 {
+			return nil
+		} else {
+			return errors.New("user wishlist is an unsupported slice")
+		}
+	} else {
+		return errors.New("user wishlist is an unknown format")
+	}
 }

go.mod
@@ -3,7 +3,7 @@ module github.com/arelate/vangogh
 go 1.24.2
 
 require (
-	github.com/arelate/southern_light v0.2.55
+	github.com/arelate/southern_light v0.2.56
 	github.com/boggydigital/atomus v0.1.10
 	github.com/boggydigital/backups v0.1.6
 	github.com/boggydigital/clo v1.0.7

go.sum
@@ -68,6 +68,8 @@ github.com/arelate/southern_light v0.2.54 h1:uJXKDNgVvxoxfglMWgEf6NvVAosv9oSIFEz
 github.com/arelate/southern_light v0.2.54/go.mod h1:g2lyyOf9XMctrXBYtjzEJF6OqzgZT1lGT/u9DWVS3T8=
 github.com/arelate/southern_light v0.2.55 h1:0ollriz6gVwjwmm9GhRvf1obRLyyJrc4JML981J/RPA=
 github.com/arelate/southern_light v0.2.55/go.mod h1:g2lyyOf9XMctrXBYtjzEJF6OqzgZT1lGT/u9DWVS3T8=
+github.com/arelate/southern_light v0.2.56 h1:u62IEaGY7LGl+5oinruZ3RvqdOgfcs0HgsuD66XwfZc=
+github.com/arelate/southern_light v0.2.56/go.mod h1:g2lyyOf9XMctrXBYtjzEJF6OqzgZT1lGT/u9DWVS3T8=
 github.com/boggydigital/atomus v0.1.10 h1:ERmg6ncLOJ/y0B13SSRAIpjDgWQfEqfeQwFBSYgrjg0=
 github.com/boggydigital/atomus v0.1.10/go.mod h1:YK4iAWR9/+2rFhfaGLjkfoFiCPY5QNu8d23I/8wuWSY=
 github.com/boggydigital/backups v0.1.6 h1:E0YU7K/muSjmJcDtMBUP3mQN46/9kkq+HczuKxRWx14=

commit 40ec9a4b11a32d74efe7aecbd2a5c8aa18deeb7c

Fixing regression introduced by https://github.com/arelate/vangogh/commit/eb581263692714a079730634251671219e8f2710 - I've assumed that the Wishlist map without type specification would be map[string]bool, and turns out it's map[string]interface{}

cli/gog_data/get_user_wishlist.go
@@ -72,11 +72,11 @@ func ReduceUserWishlist(kvUserWishlist kevlar.KeyValues) error {
 	// so in order to process the data we attempt a type conversion
 	// and handle both known data types (and fail if data has another format)
 
-	if uwlMap, ok := userWishlist.Wishlist.(map[string]bool); ok {
+	if uwlMap, ok := userWishlist.Wishlist.(map[string]any); ok {
 
 		userWishlistMap := make(map[string][]string, len(uwlMap))
 		for id, flag := range uwlMap {
-			if flag {
+			if wishlisted, sure := flag.(bool); sure && wishlisted {
 				userWishlistMap[id] = []string{vangogh_integration.TrueValue}
 			}
 		}