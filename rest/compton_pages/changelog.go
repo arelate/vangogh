12366d6a590e3cdbeb7888b9119767eb1fe4c987
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-05-08 14:12:13 -0700

    Fixing styling issues with the new changelog page

--- rest/compton_pages/changelog.go
+++ rest/compton_pages/changelog.go
@@ -2,11 +2,13 @@ package compton_pages
 
 import (
 	"github.com/arelate/southern_light/vangogh_integration"
-	"github.com/arelate/vangogh/rest/compton_fragments"
 	"github.com/arelate/vangogh/rest/compton_styles"
 	"github.com/boggydigital/compton"
 	"github.com/boggydigital/compton/consts/align"
 	"github.com/boggydigital/compton/consts/color"
+	"github.com/boggydigital/compton/consts/direction"
+	"github.com/boggydigital/compton/consts/size"
+	"github.com/boggydigital/issa"
 	"github.com/boggydigital/redux"
 	"strings"
 )
@@ -19,10 +21,26 @@ func Changelog(id string, rdx redux.Readable) compton.PageElement {
 		pageTitle = strings.Join([]string{title, "Changelog"}, " ")
 	}
 
-	p, pageStack := compton_fragments.AppPage(pageTitle)
+	p := compton.Page(pageTitle)
+
 	p.RegisterStyles(compton_styles.Styles, "changelog.css")
 
-	pageStack.Append(compton.FICenter(p, compton.HeadingText(pageTitle, 1)))
+	// tinting document background color to the representative product color
+	if imageId, ok := rdx.GetLastVal(vangogh_integration.ImageProperty, id); ok && imageId != "" {
+		if repColor, sure := rdx.GetLastVal(vangogh_integration.RepColorProperty, imageId); sure && repColor != issa.NeutralRepColor {
+			p.SetAttribute("style", "--c-rep:"+repColor)
+		}
+	}
+
+	pageStack := compton.FlexItems(p, direction.Column).
+		RowGap(size.Large)
+	p.Append(compton.FICenter(p, pageStack))
+
+	heading := compton.Heading(1)
+
+	heading.Append(compton.Fspan(p, pageTitle).TextAlign(align.Center))
+
+	pageStack.Append(compton.FICenter(p, heading))
 
 	if changelog, ok := rdx.GetAllValues(vangogh_integration.ChangelogProperty, id); !ok || len(changelog) == 0 {
 		fs := compton.Fspan(p, "Changelog is not available for this product").
@@ -30,8 +48,15 @@ func Changelog(id string, rdx redux.Readable) compton.PageElement {
 			TextAlign(align.Center)
 		pageStack.Append(compton.FICenter(p, fs))
 	} else {
+		changelogStack := compton.FlexItems(p, direction.Column).
+			AlignItems(align.Start).
+			RowGap(size.Normal).
+			MaxWidth(size.MaxWidth)
+
+		pageStack.Append(changelogStack)
+
 		for _, log := range changelog {
-			pageStack.Append(compton.Text(log))
+			changelogStack.Append(compton.Text(log))
 		}
 	}
 
