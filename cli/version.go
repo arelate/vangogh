e7394319c19629730ba7e99d476cd2936055ffcf
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-06-29 15:57:54 -0700

    Rewriting version to use nod

--- cli/version.go
+++ cli/version.go
@@ -1,21 +1,31 @@
 package cli
 
 import (
-	"fmt"
 	"github.com/arelate/vangogh/cli/reqs"
+	"github.com/boggydigital/nod"
 	"net/url"
 	"runtime/debug"
 )
 
 func VersionHandler(_ *url.URL) error {
+	va := nod.Begin("checking vangogh version...")
+	defer va.Done()
+
 	if reqs.GitTag == "" {
+		summary := make(map[string][]string)
 		if bi, ok := debug.ReadBuildInfo(); ok {
-			fmt.Println(bi)
+			values := []string{bi.Main.Version, bi.Main.Path, bi.GoVersion}
+			for _, value := range values {
+				if value != "" {
+					summary["version info:"] = append(summary["version info:"], value)
+				}
+			}
+			va.EndWithSummary("", summary)
 		} else {
-			fmt.Println("unknown version")
+			va.EndWithResult("unknown version")
 		}
 	} else {
-		fmt.Println(reqs.GitTag)
+		va.EndWithResult(reqs.GitTag)
 	}
 	return nil
 }
