6621c7136ac76894502839feb78c55ae12ef2718
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-06-28 23:44:21 -0700

    Adding version fallback from theo

--- cli/version.go
+++ cli/version.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"github.com/arelate/vangogh/cli/reqs"
 	"net/url"
+	"runtime/debug"
 )
 
 func VersionHandler(_ *url.URL) error {
 	if reqs.GitTag == "" {
-		fmt.Println("development version")
+		if bi, ok := debug.ReadBuildInfo(); ok {
+			fmt.Println(bi)
+		} else {
+			fmt.Println("unknown version")
+		}
 	} else {
 		fmt.Println(reqs.GitTag)
 	}
