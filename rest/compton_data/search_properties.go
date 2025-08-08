2b4638304c18bfb85e10ed96e6d44a8311b753d4
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-08-07 21:42:43 -0700

    Reordering search properties to be more aligned to product properties

--- rest/compton_data/search_properties.go
+++ rest/compton_data/search_properties.go
@@ -3,33 +3,23 @@ package compton_data
 import "github.com/arelate/southern_light/vangogh_integration"
 
 var SearchProperties = []string{
+
 	vangogh_integration.TitleProperty,
 	vangogh_integration.DescriptionOverviewProperty,
-	vangogh_integration.LocalTagsProperty,
-	vangogh_integration.TagIdProperty,
 	vangogh_integration.OperatingSystemsProperty,
-	vangogh_integration.HltbPlatformsProperty,
 	vangogh_integration.DevelopersProperty,
 	vangogh_integration.PublishersProperty,
-	vangogh_integration.EnginesProperty,
-	vangogh_integration.EnginesBuildsProperty,
-	vangogh_integration.SeriesProperty,
-	vangogh_integration.GenresProperty,
-	vangogh_integration.ThemesProperty,
-	vangogh_integration.HltbGenresProperty,
-	vangogh_integration.StoreTagsProperty,
-	vangogh_integration.SteamCategoriesProperty,
-	vangogh_integration.SteamDeckAppCompatibilityCategoryProperty,
-	vangogh_integration.SteamOsAppCompatibilityCategoryProperty,
-	vangogh_integration.ProtonDBTierProperty,
-	vangogh_integration.ProtonDBConfidenceProperty,
-	vangogh_integration.FeaturesProperty,
-	vangogh_integration.GameModesProperty,
-	vangogh_integration.LanguageCodeProperty,
-	vangogh_integration.ProductTypeProperty,
 
 	vangogh_integration.OwnedProperty,
+
+	vangogh_integration.TagIdProperty,
+	vangogh_integration.LocalTagsProperty,
 	vangogh_integration.UserWishlistProperty,
+
+	vangogh_integration.CreditsProperty,
+
+	vangogh_integration.ProductTypeProperty,
+	vangogh_integration.LanguageCodeProperty,
 
 	vangogh_integration.IsFreeProperty,
 	vangogh_integration.IsDemoProperty,
@@ -44,12 +34,29 @@ var SearchProperties = []string{
 	vangogh_integration.OpenCriticTierProperty,
 	vangogh_integration.TopPercentProperty,
 
-	vangogh_integration.CreditsProperty,
+	vangogh_integration.HltbPlatformsProperty,
+	vangogh_integration.EnginesProperty,
+	vangogh_integration.EnginesBuildsProperty,
+	vangogh_integration.SeriesProperty,
+	vangogh_integration.GenresProperty,
+	vangogh_integration.ThemesProperty,
+	vangogh_integration.HltbGenresProperty,
+	vangogh_integration.StoreTagsProperty,
+	vangogh_integration.FeaturesProperty,
+	vangogh_integration.GameModesProperty,
+	vangogh_integration.SteamCategoriesProperty,
 
-	vangogh_integration.GOGReleaseDateProperty,
+	vangogh_integration.SteamDeckAppCompatibilityCategoryProperty,
+	vangogh_integration.SteamOsAppCompatibilityCategoryProperty,
+	vangogh_integration.ProtonDBTierProperty,
+	vangogh_integration.ProtonDBConfidenceProperty,
+
 	vangogh_integration.GlobalReleaseDateProperty,
+	vangogh_integration.GOGReleaseDateProperty,
 	vangogh_integration.GOGOrderDateProperty,
+
 	vangogh_integration.ProductValidationResultProperty,
+
 	vangogh_integration.SortProperty,
 	vangogh_integration.DescendingProperty,
 }
