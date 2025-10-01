1db471578e043250fb8bc823b585e5e123ff5cac
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-09-30 21:28:55 -0700

    Bumping author and updating routes
    Adding /api/auth-user and /api/auth-session endpoints

--- rest/routing.go
+++ rest/routing.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	AuthSsn  = author.AuthenticateSession
+	AuthSt   = author.AuthSessionToken
 	Log      = nod.RequestLog
 	Redirect = http.RedirectHandler
 )
@@ -25,47 +25,48 @@ func HandleFuncs() {
 		"GET /atom":          Log(http.HandlerFunc(GetAtom)),
 		// public data endpoints
 		"GET /login":    Log(http.HandlerFunc(GetLogin)),
-		"POST /auth":    Log(http.HandlerFunc(sb.AuthenticateUser)),
-		"POST /success": AuthSsn(sb, Log(http.HandlerFunc(PostSuccess))),
+		"POST /auth":    Log(http.HandlerFunc(sb.AuthBrowserUsernamePassword)),
+		"POST /success": AuthSt(sb, Log(http.HandlerFunc(PostSuccess))),
 		// soon to be authenticated endpoints
-		"GET /updates":       AuthSsn(sb, Log(http.HandlerFunc(GetUpdates)), perm.ReadUpdates),
-		"GET /search":        AuthSsn(sb, Log(http.HandlerFunc(GetSearch)), perm.ReadSearch),
-		"GET /product":       AuthSsn(sb, Log(http.HandlerFunc(GetProduct)), perm.ReadProductData),
-		"GET /info":          AuthSsn(sb, Log(http.HandlerFunc(GetInfo)), perm.ReadProductData),
-		"GET /description":   AuthSsn(sb, Log(http.HandlerFunc(GetDescription)), perm.ReadProductData),
-		"GET /reception":     AuthSsn(sb, Log(http.HandlerFunc(GetReception)), perm.ReadProductData),
-		"GET /offerings":     AuthSsn(sb, Log(http.HandlerFunc(GetOfferings)), perm.ReadProductData),
-		"GET /media":         AuthSsn(sb, Log(http.HandlerFunc(GetMedia)), perm.ReadProductData),
-		"GET /news":          AuthSsn(sb, Log(http.HandlerFunc(GetNews)), perm.ReadProductData),
-		"GET /changelog":     AuthSsn(sb, Log(http.HandlerFunc(GetChangelog)), perm.ReadProductData),
-		"GET /compatibility": AuthSsn(sb, Log(http.HandlerFunc(GetCompatibility)), perm.ReadProductData),
-		"GET /installers":    AuthSsn(sb, Log(http.HandlerFunc(GetInstallers)), perm.ReadFiles),
-		"GET /wine-binaries": AuthSsn(sb, Log(http.HandlerFunc(GetWineBinaries)), perm.ReadFiles),
+		"GET /updates":       AuthSt(sb, Log(http.HandlerFunc(GetUpdates)), perm.ReadUpdates),
+		"GET /search":        AuthSt(sb, Log(http.HandlerFunc(GetSearch)), perm.ReadSearch),
+		"GET /product":       AuthSt(sb, Log(http.HandlerFunc(GetProduct)), perm.ReadProductData),
+		"GET /info":          AuthSt(sb, Log(http.HandlerFunc(GetInfo)), perm.ReadProductData),
+		"GET /description":   AuthSt(sb, Log(http.HandlerFunc(GetDescription)), perm.ReadProductData),
+		"GET /reception":     AuthSt(sb, Log(http.HandlerFunc(GetReception)), perm.ReadProductData),
+		"GET /offerings":     AuthSt(sb, Log(http.HandlerFunc(GetOfferings)), perm.ReadProductData),
+		"GET /media":         AuthSt(sb, Log(http.HandlerFunc(GetMedia)), perm.ReadProductData),
+		"GET /news":          AuthSt(sb, Log(http.HandlerFunc(GetNews)), perm.ReadProductData),
+		"GET /changelog":     AuthSt(sb, Log(http.HandlerFunc(GetChangelog)), perm.ReadProductData),
+		"GET /compatibility": AuthSt(sb, Log(http.HandlerFunc(GetCompatibility)), perm.ReadProductData),
+		"GET /installers":    AuthSt(sb, Log(http.HandlerFunc(GetInstallers)), perm.ReadFiles),
+		"GET /wine-binaries": AuthSt(sb, Log(http.HandlerFunc(GetWineBinaries)), perm.ReadFiles),
 		// public media endpoints
-		"GET /image":               AuthSsn(sb, Log(http.HandlerFunc(GetImage)), perm.ReadImages),
-		"GET /description-images/": AuthSsn(sb, Log(http.HandlerFunc(GetDescriptionImages)), perm.ReadImages),
+		"GET /image":               AuthSt(sb, Log(http.HandlerFunc(GetImage)), perm.ReadImages),
+		"GET /description-images/": AuthSt(sb, Log(http.HandlerFunc(GetDescriptionImages)), perm.ReadImages),
 		// public files endpoints
-		"GET /wine-binary-file": AuthSsn(sb, Log(http.HandlerFunc(GetWineBinaryFile)), perm.ReadFiles),
+		"GET /wine-binary-file": AuthSt(sb, Log(http.HandlerFunc(GetWineBinaryFile)), perm.ReadFiles),
 		// auth data endpoints
-		"GET /wishlist/add":     AuthSsn(sb, Log(http.HandlerFunc(GetWishlistAdd)), perm.WriteWishlist),
-		"GET /wishlist/remove":  AuthSsn(sb, Log(http.HandlerFunc(GetWishlistRemove)), perm.WriteWishlist),
-		"GET /tags/edit":        AuthSsn(sb, Log(http.HandlerFunc(GetTagsEdit)), perm.WriteAccountTags),
-		"GET /tags/apply":       AuthSsn(sb, Log(http.HandlerFunc(GetTagsApply)), perm.WriteAccountTags),
-		"GET /local-tags/edit":  AuthSsn(sb, Log(http.HandlerFunc(GetLocalTagsEdit)), perm.WriteLocalTags),
-		"GET /local-tags/apply": AuthSsn(sb, Log(http.HandlerFunc(GetLocalTagsApply)), perm.WriteLocalTags),
+		"GET /wishlist/add":     AuthSt(sb, Log(http.HandlerFunc(GetWishlistAdd)), perm.WriteWishlist),
+		"GET /wishlist/remove":  AuthSt(sb, Log(http.HandlerFunc(GetWishlistRemove)), perm.WriteWishlist),
+		"GET /tags/edit":        AuthSt(sb, Log(http.HandlerFunc(GetTagsEdit)), perm.WriteAccountTags),
+		"GET /tags/apply":       AuthSt(sb, Log(http.HandlerFunc(GetTagsApply)), perm.WriteAccountTags),
+		"GET /local-tags/edit":  AuthSt(sb, Log(http.HandlerFunc(GetLocalTagsEdit)), perm.WriteLocalTags),
+		"GET /local-tags/apply": AuthSt(sb, Log(http.HandlerFunc(GetLocalTagsApply)), perm.WriteLocalTags),
 		// auth files endpoints
-		"GET /files": AuthSsn(sb, Log(http.HandlerFunc(GetFiles)), perm.ReadFiles),
+		"GET /files": AuthSt(sb, Log(http.HandlerFunc(GetFiles)), perm.ReadFiles),
 		// products redirects
 		"GET /products": Redirect("/search", http.StatusPermanentRedirect),
 		// API
-		"POST /api/auth":                  Log(http.HandlerFunc(sb.AuthenticateApi)),
-		"GET /api/health":                 AuthSsn(sb, Log(http.HandlerFunc(GetHealth)), perm.ReadApi),
-		"GET /api/product-details":        AuthSsn(sb, Log(http.HandlerFunc(GetProductDetails)), perm.ReadApi),
-		"GET /api/wine-binaries-versions": AuthSsn(sb, Log(http.HandlerFunc(GetWineBinariesVersions)), perm.ReadApi),
+		"POST /api/auth-user":             Log(http.HandlerFunc(sb.AuthApiUsernamePassword)),
+		"POST /api/auth-session":          Log(http.HandlerFunc(sb.AuthApiSession)),
+		"GET /api/health":                 AuthSt(sb, Log(http.HandlerFunc(GetHealth)), perm.ReadApi),
+		"GET /api/product-details":        AuthSt(sb, Log(http.HandlerFunc(GetProductDetails)), perm.ReadApi),
+		"GET /api/wine-binaries-versions": AuthSt(sb, Log(http.HandlerFunc(GetWineBinariesVersions)), perm.ReadApi),
 		// debug endpoints
-		"GET /debug":      AuthSsn(sb, Log(http.HandlerFunc(GetDebug)), perm.ReadDebug),
-		"GET /debug-data": AuthSsn(sb, Log(http.HandlerFunc(GetDebugData)), perm.ReadDebug),
-		"GET /logs":       AuthSsn(sb, Log(http.HandlerFunc(GetLogs)), perm.ReadLogs),
+		"GET /debug":      AuthSt(sb, Log(http.HandlerFunc(GetDebug)), perm.ReadDebug),
+		"GET /debug-data": AuthSt(sb, Log(http.HandlerFunc(GetDebugData)), perm.ReadDebug),
+		"GET /logs":       AuthSt(sb, Log(http.HandlerFunc(GetLogs)), perm.ReadLogs),
 		// start at the updates
 		"GET /": Redirect("/updates", http.StatusPermanentRedirect),
 	}
