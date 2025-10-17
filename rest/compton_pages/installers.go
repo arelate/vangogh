9983e9c8ca4fb92ca0eb88f6a9c0b7bd4e50ce49
Bogdan Brinza <bogdan.brinza@hey.com>
2025-10-17 13:32:37 -0700

    Rolling back the last change

--- rest/compton_pages/installers.go
+++ rest/compton_pages/installers.go
@@ -256,22 +256,7 @@ func downloadLink(r compton.Registrar,
 
 	dvs := vangogh_integration.NewDvs(dl.ManualUrl, rdx)
 
-	var downloadValidationStatus string
-
-	if dl.Type == vangogh_integration.Installer || dl.Type == vangogh_integration.DLC {
-
-		switch dvs.ValidationResult() {
-		case vangogh_integration.ValidationResultUnknown:
-			downloadValidationStatus = dvs.ManualUrlStatus().HumanReadableString()
-		default:
-			downloadValidationStatus = dvs.ValidationResult().HumanReadableString()
-		}
-
-	} else {
-		downloadValidationStatus = dvs.ManualUrlStatus().HumanReadableString()
-	}
-
-	manualUrlStatusValidationResult := compton.Fspan(r, downloadValidationStatus).
+	manualUrlStatusValidationResult := compton.Fspan(r, dvs.HumanReadableString()).
 		FontSize(size.XSmall).
 		ForegroundColor(compton_fragments.ValidationResultsColors[dvs.ValidationResult()]).
 		FontWeight(validationResultsFontWeights[dvs.ValidationResult()])
