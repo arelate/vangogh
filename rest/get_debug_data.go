e1d29ef238d2fd71ef109ec46f6b68b7e403f864
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-11-09 16:44:50 -0800

    Refreshing debug data, not just debug page

--- rest/get_debug_data.go
+++ rest/get_debug_data.go
@@ -1,15 +1,21 @@
 package rest
 
 import (
+	"net/http"
+
 	"github.com/arelate/southern_light/vangogh_integration"
 	"github.com/arelate/vangogh/rest/compton_pages"
 	"github.com/boggydigital/nod"
-	"net/http"
 )
 
 func GetDebugData(w http.ResponseWriter, r *http.Request) {
 
 	// GET /debug-data?id&product-type
+
+	if err := RefreshRedux(); err != nil {
+		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
+		return
+	}
 
 	id := r.URL.Query().Get(vangogh_integration.IdProperty)
 	pts := r.URL.Query().Get(vangogh_integration.ProductTypeProperty)
