71286e87ef8e7fcd64fea75a85d9381933760a5a
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-05-20 20:34:33 -0700

    Handling errors that are different from no users in login

---
+++ rest/compton_pages/error.go
@@ -0,0 +1,32 @@
+package compton_pages
+
+import (
+	"github.com/arelate/vangogh/rest/compton_fragments"
+	"github.com/boggydigital/compton"
+	"github.com/boggydigital/compton/consts/direction"
+	"github.com/boggydigital/compton/consts/size"
+)
+
+func Error(err error) compton.PageElement {
+
+	const (
+		pageTitle = "Error"
+	)
+
+	p := compton.Page(pageTitle)
+	pageStack := compton.FlexItems(p, direction.Column).RowGap(size.Large)
+	p.Append(pageStack)
+
+	headingsStack := compton.FlexItems(p, direction.Column).RowGap(size.Small)
+	titleHeading := compton.H2Text(pageTitle)
+
+	headingsStack.Append(titleHeading)
+
+	pageStack.Append(compton.Div(), compton.FICenter(p, headingsStack))
+
+	pageStack.Append(compton.FICenter(p, compton.SpanText(err.Error())))
+
+	pageStack.Append(compton.FICenter(p, compton_fragments.GitHubLink(p)))
+
+	return p
+}
--- rest/get_login.go
+++ rest/get_login.go
@@ -1,9 +1,11 @@
 package rest
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/arelate/vangogh/rest/compton_pages"
+	"github.com/boggydigital/author"
 	"github.com/boggydigital/compton"
 	"github.com/boggydigital/nod"
 )
@@ -12,15 +14,15 @@ func GetLogin(w http.ResponseWriter, r *http.Request) {
 
 	var p compton.PageElement
 
-	err := sb.MustHaveUsers()
-	switch err {
-	case nil:
+	if err := sb.MustHaveUsers(); err == nil {
 		p = compton_pages.Login("/auth")
-	default:
+	} else if errors.Is(err, author.ErrNoUsers) {
 		p = compton_pages.NoUsers()
+	} else {
+		p = compton_pages.Error(err)
 	}
 
-	if err = p.WriteResponse(w); err != nil {
+	if err := p.WriteResponse(w); err != nil {
 		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
 	}
 
