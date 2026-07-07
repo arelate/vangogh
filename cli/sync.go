ab4d8a5fa0aba9b8dd9f056be5a0133312cdc67e
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-07-06 20:34:57 -0700

    Refactoring sync to remove outdated options and make behavior more predictable. Just running sync (with no other parameters) will get purchases/account updates. Other than the common downloads parameters (OS, lang-code, no-dlc, no-extras, no-patches, downloads-layout) the only real parameter is all-data that would download all available additional data and images from that data

--- cli/sync.go
+++ cli/sync.go
@@ -6,7 +6,6 @@ import (
 	"net/url"
 	"slices"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/arelate/southern_light/gog_integration"
@@ -17,68 +16,7 @@ import (
 	"github.com/boggydigital/nod"
 )
 
-const (
-	SyncOptionPurchases         = "purchases-data"
-	SyncOptionsExtraData        = "extra-data"
-	SyncOptionDescriptionImages = "description-images"
-	SyncOptionImages            = "images"
-	SyncOptionScreenshots       = "screenshots"
-	SyncOptionVideosMetadata    = "videos-metadata"
-	SyncOptionDownloadsUpdates  = "downloads-updates"
-	SynOptionBinaries           = "binaries"
-	negativePrefix              = "no-"
-)
-
-type syncOptions struct {
-	purchasesData     bool
-	extraData         bool
-	descriptionImages bool
-	images            bool
-	screenshots       bool
-	videosMetadata    bool
-	downloadsUpdates  bool
-	binaries          bool
-}
-
-func NegOpt(option string) string {
-	if !strings.HasPrefix(option, negativePrefix) {
-		return negativePrefix + option
-	}
-	return option
-}
-
-func initSyncOptions(u *url.URL) *syncOptions {
-
-	q := u.Query()
-
-	so := &syncOptions{
-		purchasesData:     q.Has(SyncOptionPurchases),
-		extraData:         q.Has(SyncOptionsExtraData),
-		descriptionImages: q.Has(SyncOptionDescriptionImages),
-		images:            q.Has(SyncOptionImages),
-		screenshots:       q.Has(SyncOptionScreenshots),
-		videosMetadata:    q.Has(SyncOptionVideosMetadata),
-		downloadsUpdates:  q.Has(SyncOptionDownloadsUpdates),
-		binaries:          q.Has(SynOptionBinaries),
-	}
-
-	if q.Has(vangogh_integration.UrlAllParameter) {
-		so.purchasesData = !q.Has(NegOpt(SyncOptionPurchases))
-		so.extraData = !q.Has(NegOpt(SyncOptionsExtraData))
-		so.descriptionImages = !q.Has(NegOpt(SyncOptionDescriptionImages))
-		so.images = !q.Has(NegOpt(SyncOptionImages))
-		so.screenshots = !q.Has(NegOpt(SyncOptionScreenshots))
-		so.videosMetadata = !q.Has(NegOpt(SyncOptionVideosMetadata))
-		so.downloadsUpdates = !q.Has(NegOpt(SyncOptionDownloadsUpdates))
-		so.binaries = !q.Has(NegOpt(SynOptionBinaries))
-	}
-
-	return so
-}
-
 func SyncHandler(u *url.URL) error {
-	syncOpts := initSyncOptions(u)
-
 	since, err := vangogh_integration.SinceHouseAgoFromUrl(u)
 	if err != nil {
 		return err
@@ -88,26 +26,27 @@ func SyncHandler(u *url.URL) error {
 
 	return Sync(
 		since,
-		syncOpts,
 		vangogh_integration.OperatingSystemsFromUrl(u),
 		vangogh_integration.LanguageCodesFromUrl(u),
 		q.Has(vangogh_integration.UrlNoDlcsParameter),
 		q.Has(vangogh_integration.UrlNoExtrasParameter),
 		q.Has(vangogh_integration.UrlNoPatchesParameter),
 		vangogh_integration.DownloadsLayoutFromUrl(u),
-		!q.Has(vangogh_integration.UrlNoCleanupParameter),
+		q.Has(vangogh_integration.UrlAllDataParameter),
 		q.Has(vangogh_integration.UrlForceParameter))
 }
 
 func Sync(
 	since int64,
-	syncOpts *syncOptions,
 	operatingSystems []vangogh_integration.OperatingSystem,
 	langCodes []string,
 	noDlcs, noExtras, noPatches bool,
 	downloadsLayout vangogh_integration.DownloadsLayout,
-	cleanup bool,
+	allData bool,
 	force bool) error {
+
+	// sync sequence:
+	// 1.
 
 	sa := nod.Begin("syncing data...")
 	defer sa.Done()
@@ -117,18 +56,10 @@ func Sync(
 		return err
 	}
 
-	utcNow := time.Now().UTC().Unix()
-	// since-hours-ago specifies hours,
-	// so anything less than an hour in seconds would be default value
-	if utcNow-since < 60*60 {
-		if lscs, ok := syncEventsRdx.GetLastVal(vangogh_integration.VangoghSyncEventsProperty, vangogh_integration.SyncCompleteKey); ok && lscs != "" {
-			var lsci int64
-			if lsci, err = strconv.ParseInt(lscs, 10, 64); err == nil {
-				since = lsci
-			} else {
-				return setSyncInterrupted(err, syncEventsRdx)
-			}
-		}
+	// setting since to last sync completed to be able to capture DLCs
+	// purchased between syncs
+	if since, err = setSinceToLastSyncCompleted(since, syncEventsRdx); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
 	}
 
 	syncStart := since
@@ -140,122 +71,109 @@ func Sync(
 		return setSyncInterrupted(err, syncEventsRdx)
 	}
 
-	if syncOpts.purchasesData {
-		if err = GetData(nil, nil, since, new(dataFilter{purchases: true}), force); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
-
+	if err = GetData(nil, nil, since, new(dataFilter{purchases: true}), force); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	} else {
 		if err = setSyncEvent(vangogh_integration.SyncPurchasesDataKey, syncEventsRdx); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
-
-		// summarize sync updates now, since other updates are digital artifacts
-		// and won't affect the summaries
-		if err = Summarize(syncStart); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
 		}
 	}
 
+	// summarize sync updates now, since other updates are digital artifacts
+	// and won't affect the summaries
+	if err = Summarize(syncStart); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	}
+
 	// get images
-	if syncOpts.images {
-
-		if err = GetImages(nil, gog_integration.AllImageTypes(), true, force); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
-
+	if err = GetImages(nil, gog_integration.AllImageTypes(), true, force); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	} else {
 		if err = setSyncEvent(vangogh_integration.SyncPurchasesImagesKey, syncEventsRdx); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
 		}
 	}
 
 	// get downloads updates
-	if syncOpts.downloadsUpdates {
 
-		var updatedDetails map[string]any
-		updatedDetails, err = shared_data.GetGogDetailsUpdates(since)
-		if err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
+	var updatedDetails map[string]any
+	updatedDetails, err = shared_data.GetGogDetailsUpdates(since)
+	if err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	}
 
-		if err = queueDownloads(maps.Keys(updatedDetails)); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
+	if err = queueDownloads(maps.Keys(updatedDetails)); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	}
 
-		if err = GetDownloads(nil,
-			operatingSystems,
-			langCodes,
-			noDlcs,
-			noExtras,
-			noPatches,
-			downloadsLayout,
-			&getDownloadOptions{
-				validate: true,
-				queued:   true,
-				force:    true,
-			}); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
-
+	if err = GetDownloads(nil,
+		operatingSystems,
+		langCodes,
+		noDlcs,
+		noExtras,
+		noPatches,
+		downloadsLayout,
+		&getDownloadOptions{
+			validate: true,
+			queued:   true,
+			force:    true,
+		}); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	} else {
 		if err = setSyncEvent(vangogh_integration.SyncDownloadsKey, syncEventsRdx); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
 		}
+	}
 
-		if err = GenerateMissingChecksums(operatingSystems, langCodes, noDlcs, noExtras, noPatches, downloadsLayout); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
-
+	if err = GenerateMissingChecksums(operatingSystems, langCodes, noDlcs, noExtras, noPatches, downloadsLayout); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	} else {
 		if err = setSyncEvent(vangogh_integration.SyncGenerateMissingChecksums, syncEventsRdx); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
 		}
-
-		if cleanup {
-			if err = Cleanup(nil,
-				operatingSystems,
-				langCodes,
-				noDlcs,
-				noExtras,
-				noPatches,
-				downloadsLayout,
-				true,
-				false); err != nil {
-				return setSyncInterrupted(err, syncEventsRdx)
-			}
-
-			if err = setSyncEvent(vangogh_integration.SyncCleanupKey, syncEventsRdx); err != nil {
-				return setSyncInterrupted(err, syncEventsRdx)
-			}
-		}
-
 	}
 
-	if syncOpts.videosMetadata {
-		if err = GetVideoMetadata(nil, true, false); err != nil {
+	if err = Cleanup(nil,
+		operatingSystems,
+		langCodes,
+		noDlcs,
+		noExtras,
+		noPatches,
+		downloadsLayout,
+		true,
+		false); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	} else {
+		if err = setSyncEvent(vangogh_integration.SyncCleanupKey, syncEventsRdx); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
 		}
+	}
 
+	if err = GetVideoMetadata(nil, true, false); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	} else {
 		if err = setSyncEvent(vangogh_integration.SyncVideoMetadataKey, syncEventsRdx); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
 		}
 	}
 
-	if syncOpts.binaries {
-		if err = GetBinaries(operatingSystems, true, true, force); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
-
+	if err = GetBinaries(operatingSystems, true, true, force); err != nil {
+		return setSyncInterrupted(err, syncEventsRdx)
+	} else {
 		if err = setSyncEvent(vangogh_integration.SyncBinaries, syncEventsRdx); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
 		}
 	}
 
-	// getting extra data
-	if syncOpts.extraData {
+	// getting additional data
+	if allData {
+
 		if err = GetData(nil, nil, since, new(dataFilter{extraData: true, relatedApiProducts: true}), force); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
-		}
-
-		if err = setSyncEvent(vangogh_integration.SyncExtraData, syncEventsRdx); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
+		} else {
+			if err = setSyncEvent(vangogh_integration.SyncExtraData, syncEventsRdx); err != nil {
+				return setSyncInterrupted(err, syncEventsRdx)
+			}
 		}
 
 		// summarize extra data updates now, since other updates are digital artifacts
@@ -263,38 +181,34 @@ func Sync(
 		if err = Summarize(syncStart); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
 		}
-	}
 
-	// get extra images
-	if syncOpts.images {
-
+		// get extra images
 		if err = GetImages(nil, gog_integration.AllImageTypes(), true, force); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
+		} else {
+			if err = setSyncEvent(vangogh_integration.SyncExtraImagesKey, syncEventsRdx); err != nil {
+				return setSyncInterrupted(err, syncEventsRdx)
+			}
 		}
 
-		if err = setSyncEvent(vangogh_integration.SyncExtraImagesKey, syncEventsRdx); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
-	}
-
-	// get description images
-	if syncOpts.descriptionImages {
+		// get description images
 		if err = GetDescriptionImages(nil, since, false, force); err != nil {
 			return setSyncInterrupted(err, syncEventsRdx)
+		} else {
+			if err = setSyncEvent(vangogh_integration.SyncDescriptionImagesKey, syncEventsRdx); err != nil {
+				return setSyncInterrupted(err, syncEventsRdx)
+			}
 		}
 
-		if err = setSyncEvent(vangogh_integration.SyncDescriptionImagesKey, syncEventsRdx); err != nil {
-			return setSyncInterrupted(err, syncEventsRdx)
-		}
 	}
 
 	// backing up data
 	if err = Backup(); err != nil {
 		return setSyncInterrupted(err, syncEventsRdx)
-	}
-
-	if err = setSyncEvent(vangogh_integration.SyncBackup, syncEventsRdx); err != nil {
-		return setSyncInterrupted(err, syncEventsRdx)
+	} else {
+		if err = setSyncEvent(vangogh_integration.SyncBackup, syncEventsRdx); err != nil {
+			return setSyncInterrupted(err, syncEventsRdx)
+		}
 	}
 
 	// print new, updated
@@ -331,4 +245,22 @@ func setSyncInterrupted(err error, rdx redux.Writeable) error {
 	}
 
 	return err
+}
+
+func setSinceToLastSyncCompleted(since int64, syncEventsRdx redux.Readable) (int64, error) {
+
+	utcNow := time.Now().UTC().Unix()
+	// since-hours-ago specifies hours,
+	// so anything less than an hour in seconds would be default value
+	if utcNow-since < 60*60 {
+		if lscs, ok := syncEventsRdx.GetLastVal(vangogh_integration.VangoghSyncEventsProperty, vangogh_integration.SyncCompleteKey); ok && lscs != "" {
+			if lsci, err := strconv.ParseInt(lscs, 10, 64); err == nil {
+				since = lsci
+			} else {
+				return -1, err
+			}
+		}
+	}
+
+	return since, nil
 }
--- clo_delegates/func_map.go
+++ clo_delegates/func_map.go
@@ -6,42 +6,17 @@ import (
 
 	"github.com/arelate/southern_light/gog_integration"
 	"github.com/arelate/southern_light/vangogh_integration"
-	"github.com/arelate/vangogh/cli"
 	"github.com/arelate/vangogh/perm"
 )
 
 var FuncMap = map[string]func() []string{
-	"all-product-types":   vangogh_integration.ProductTypesCloValues,
+	"product-types":       vangogh_integration.ProductTypesCloValues,
 	"image-types":         gog_integration.ImageTypesCloValues,
-	"redux-properties":    vangogh_integration.ReduxProperties,
 	"operating-systems":   vangogh_integration.OperatingSystemsCloValues,
 	"downloads-layouts":   vangogh_integration.DownloadsLayoutsCloValues,
 	"language-codes":      gog_integration.LanguageCodesCloValues,
 	"validation-statuses": vangogh_integration.AllValidationStatusCloValues,
-	"sync-options":        syncOptions,
 	"roles":               roles,
-}
-
-func options(opts []string) []string {
-	excludeOptions := make([]string, len(opts))
-	for i, opt := range opts {
-		excludeOptions[i] = cli.NegOpt(opt)
-	}
-
-	opts = append([]string{"all"}, opts...)
-	return append(opts, excludeOptions...)
-}
-
-func syncOptions() []string {
-	return options([]string{
-		cli.SyncOptionPurchases,
-		cli.SyncOptionDescriptionImages,
-		cli.SyncOptionImages,
-		cli.SyncOptionScreenshots,
-		cli.SyncOptionVideosMetadata,
-		cli.SyncOptionDownloadsUpdates,
-		cli.SynOptionBinaries,
-	})
 }
 
 func roles() []string {
