d42bf86a9279f12c496938854453144aa42f574f
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-08-09 21:19:56 -0700

    Defaulting to /gog/dispatch to simplify redirects

--- rest/routing.go
+++ rest/routing.go
@@ -80,8 +80,7 @@ func HandleFuncs() {
 		"GET /import-cookies":  AuthCookie(sb, Log(http.HandlerFunc(GetImportCookies)), perm.WriteCookies),
 		"POST /import-cookies": AuthCookie(sb, Log(http.HandlerFunc(PostImportCookies)), perm.WriteCookies),
 		// start at the updates
-		"GET /":    Redirect("/gog", http.StatusPermanentRedirect),
-		"GET /gog": Redirect("/gog/dispatch", http.StatusPermanentRedirect),
+		"GET /": Redirect("/gog/dispatch", http.StatusPermanentRedirect),
 	}
 
 	for p, h := range patternHandlers {
