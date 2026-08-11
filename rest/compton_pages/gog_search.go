616b3ba96a79cba2e094856e11a044ae79bc8637
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-08-10 21:45:58 -0700

    Restoring search query simplifaction logic

--- rest/compton_pages/gog_search.go
+++ rest/compton_pages/gog_search.go
@@ -5,8 +5,8 @@ import (
 	"net/url"
 	"slices"
 	"strconv"
-	"strings"
 
+	"github.com/arelate/southern_light/vangogh_integration"
 	"github.com/arelate/vangogh/rest/compton_data"
 	"github.com/arelate/vangogh/rest/compton_fragments"
 	"github.com/boggydigital/author"
@@ -16,13 +16,32 @@ import (
 	"github.com/boggydigital/redux"
 )
 
+const (
+	SearchResultsLimit = 60 // divisible by 2,3,4,5,6
+)
+
+var gogSectionTitles = map[string]string{
+	"/gog/search":   "Search",
+	"/gog/owned":    "Owned",
+	"/gog/catalog":  "Catalog",
+	"/gog/wishlist": "Wishlist",
+	"/gog/sale":     "Sale",
+}
+
 const filterSearchTitle = "Filter & search"
 
-func GogSearch(section string, query map[string][]string, ids []string, from, to int, rdx redux.Readable, permissions ...author.Permission) compton.PageElement {
+func GogSearch(sectionUrl string, query url.Values, rdx redux.Readable, permissions ...author.Permission) compton.PageElement {
 
 	query = permittedQuery(query, permissions...)
 
-	p, pageStack := compton_fragments.AppPage("GOG Search")
+	maps.Copy(query, searchQueryByUrl(sectionUrl))
+
+	sectionTitle := "vangogh"
+	if st, ok := gogSectionTitles[sectionUrl]; ok {
+		sectionTitle = st
+	}
+
+	p, pageStack := compton_fragments.AppPage(sectionTitle)
 
 	p.AppendSpeculationRules(compton.SpeculationRulesConservativeEagerness, "/*")
 
@@ -30,7 +49,7 @@ func GogSearch(section string, query map[string][]string, ids []string, from, to
 
 	pageStack.Append(compton.FICenter(p,
 		compton_fragments.DistributorNavLinks(p, "GOG"),
-		compton_fragments.GogNavLinks(p, section)))
+		compton_fragments.GogNavLinks(p, sectionTitle)))
 
 	/* Filter & Search details */
 
@@ -39,6 +58,11 @@ func GogSearch(section string, query map[string][]string, ids []string, from, to
 		SummaryMarginBlockEnd(size.Normal).
 		DetailsMarginBlockEnd(size.Unset).
 		SummaryRowGap(size.XXSmall)
+
+	ids, from, to, err := searchResults(query, rdx)
+	if err != nil {
+		return p.Error(err)
+	}
 
 	if len(query) > 0 {
 
@@ -86,14 +110,20 @@ func GogSearch(section string, query map[string][]string, ids []string, from, to
 	/* Show more... button */
 
 	if to < len(ids) {
-		query["from"] = []string{strconv.Itoa(to)}
-		enq := encodeQuery(query)
+
+		nextQuery := make(url.Values)
+
+		if sectionUrl == "/gog/search" {
+			maps.Copy(nextQuery, query)
+		}
+
+		nextQuery.Set(vangogh_integration.UrlFromParameter, strconv.Itoa(to))
 
 		backToTopNavLinks := compton.NavLinks(p)
 		backToTopNavLinks.AppendLink(p, &compton.NavTarget{Href: "#_top", Title: "Back to top"})
 
 		nextPageNavLink := compton.NavLinks(p)
-		nextPageNavLink.AppendLink(p, &compton.NavTarget{Href: "/gog/search?" + enq, Title: "Next page"})
+		nextPageNavLink.AppendLink(p, &compton.NavTarget{Href: sectionUrl + "?" + nextQuery.Encode(), Title: "Next page"})
 
 		pageStack.Append(compton.FICenter(p, backToTopNavLinks, nextPageNavLink).ColumnGap(size.Small))
 	}
@@ -107,13 +137,72 @@ func GogSearch(section string, query map[string][]string, ids []string, from, to
 	return p
 }
 
-func encodeQuery(query map[string][]string) string {
-	q := &url.Values{}
-	for property, values := range query {
-		q.Set(property, strings.Join(values, ", "))
+func searchResults(query url.Values, rdx redux.Readable) (ids []string, from int, to int, err error) {
+
+	if len(query) == 0 {
+		return nil, 0, 0, nil
 	}
 
-	return q.Encode()
+	sort := query.Get(vangogh_integration.UrlSortParameter)
+	if sort == "" {
+		sort = vangogh_integration.GogTitleProperty
+	}
+	desc := query.Get(vangogh_integration.UrlDescendingParameter) == "true"
+
+	var found []string
+
+	if isSortDescOnly(query) {
+		found = slices.Collect(rdx.Keys(vangogh_integration.GogTitleProperty))
+	} else {
+		found = slices.Collect(rdx.Match(query))
+	}
+
+	ids, err = rdx.Sort(found, desc, sort, vangogh_integration.GogTitleProperty, vangogh_integration.GogProductTypeProperty)
+	if err != nil {
+		return nil, 0, 0, err
+	}
+
+	if fromStr := query.Get("from"); fromStr != "" {
+		if from, err = strconv.Atoi(fromStr); err != nil {
+			return nil, 0, 0, err
+		}
+	}
+
+	if from > len(ids)-1 {
+		from = 0
+	}
+
+	to = from + SearchResultsLimit
+	if to > len(ids) {
+		to = len(ids)
+	} else if to+SearchResultsLimit > len(ids) {
+		to = len(ids)
+	}
+
+	return ids, from, to, nil
+}
+
+func isSortDescOnly(q map[string][]string) bool {
+	switch len(q) {
+	case 0:
+		return false
+	case 1:
+		_, okSort := q[vangogh_integration.UrlSortParameter]
+		_, okDesc := q[vangogh_integration.UrlDescendingParameter]
+		return okSort || okDesc
+	case 2:
+		_, okSort := q[vangogh_integration.UrlSortParameter]
+		_, okDesc := q[vangogh_integration.UrlDescendingParameter]
+		return okSort && okDesc
+	case 3:
+		_, okSort := q[vangogh_integration.UrlSortParameter]
+		_, okDesc := q[vangogh_integration.UrlDescendingParameter]
+		_, okFrom := q[vangogh_integration.UrlFromParameter]
+		return okSort && okDesc && okFrom
+	default:
+		return false
+	}
+
 }
 
 func permittedQuery(query map[string][]string, permissions ...author.Permission) map[string][]string {
@@ -130,4 +219,28 @@ func permittedQuery(query map[string][]string, permissions ...author.Permission)
 	}
 
 	return permQuery
+}
+
+func searchQueryByUrl(sectionUrl string) url.Values {
+	q := make(url.Values)
+
+	switch sectionUrl {
+	case "/gog/owned":
+		q.Set(vangogh_integration.GogIsAccountProductProperty, vangogh_integration.TrueValue)
+		q.Set(vangogh_integration.UrlSortParameter, vangogh_integration.GogAccountProductOrderProperty)
+	case "/gog/sale":
+		q.Set(vangogh_integration.GogOwnedProperty, vangogh_integration.FalseValue)
+		q.Set(vangogh_integration.GogIsDiscountedProperty, vangogh_integration.TrueValue)
+		q.Set(vangogh_integration.UrlSortParameter, vangogh_integration.GogDiscountPercentageProperty)
+		q.Set(vangogh_integration.UrlDescendingParameter, vangogh_integration.TrueValue)
+	case "/gog/wishlist":
+		q.Set(vangogh_integration.GogUserWishlistProperty, vangogh_integration.TrueValue)
+		q.Set(vangogh_integration.UrlSortParameter, vangogh_integration.GogReleaseDateProperty)
+		q.Set(vangogh_integration.UrlDescendingParameter, vangogh_integration.TrueValue)
+	case "/gog/catalog":
+		q.Set(vangogh_integration.UrlSortParameter, vangogh_integration.GogReleaseDateProperty)
+		q.Set(vangogh_integration.UrlDescendingParameter, vangogh_integration.TrueValue)
+	}
+
+	return q
 }
